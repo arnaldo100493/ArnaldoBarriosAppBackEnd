d7fbd6e08875f5d3103e11e6650a86fc98e51bfc
Arnaldo Andrés Barrios Mena <abarrios@cobiscorp.com>
2020-08-20 18:19:47 -0500

    Ajustes Script SQL

--- ScriptsSQL/dbarnaldobarriosapp.sql
+++ ScriptsSQL/dbarnaldobarriosapp.sql
@@ -3,7 +3,7 @@ drop database if exists dbarnaldobarriosapp;
 create database if not exists dbarnaldobarriosapp;
 use dbarnaldobarriosapp;
 
-create table persona(
+create table if not exists persona(
    idPersona integer not null primary key,
    nombre varchar(1000) not null,
    apellido varchar(1000) not null,
